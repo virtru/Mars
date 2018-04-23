2daaf2a941e60fa64171f53b77cf75941c6f55c7
Sujan Kota <sujankota@gmail.com>
2018-04-23 15:38:02 -0700

    + More debug log to track DB lock bugs.

--- Mars/Mars/MConnection.h
+++ Mars/Mars/MConnection.h
@@ -23,6 +23,7 @@
 @interface MConnection : NSObject
 @property (nonatomic, assign, readonly) sqlite3 *dbHandle;
 @property (nonatomic, assign, readonly) int64_t lastInsertRowId;
+@property (nonatomic, strong) NSMutableArray* sQueryList;
 
 - (id)init;
 - (id)initWithPath:(NSString *)path;
